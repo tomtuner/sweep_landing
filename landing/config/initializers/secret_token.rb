70335895c6fb8acc76ab3fac97e793bfa6a01d20
Ian Young <ianyoung.work@gmail.com>
2013-07-09 16:01:29 -0400

    Created the landing stuff

---
+++ landing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landing::Application.config.secret_token = '8e19a41e226682c7eea12a9205b85e25f28ecf604a02460780ba2c7114c089458e556260d94c0f2dee9c5b61ce42af79aa54f5fd51ba1f5a3fe4b0e97a5d7fe2'
